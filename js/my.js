c47b454f8f30f369b57c4272c51c7facc048e0c4
bikal1000 <bikal1000.bs@gmail.com>
2018-11-14 17:49:16 +0545

    online shop

---
+++ js/my.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        mode: 'horizontal',
+        auto: true,
+    });
+    $('.menu-togglr').on('click', function () {
+        $('#mainav').slideToggle('fast');
+        $(this).toggleClass('active');
+    });
+});
